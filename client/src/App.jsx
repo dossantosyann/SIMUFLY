ff138f8d98426fcf892a361792d407b92a507bbb
Yann Dos Santos <yann@MacBook-Pro-de-Yann.local>
2025-03-25 18:17:22 +0100

    tests et modifications

--- client/src/App.jsx
+++ client/src/App.jsx
@@ -1,6 +1,5 @@
 import { useState, useEffect } from 'react'
-import reactLogo from './assets/react.svg'
-import viteLogo from '/vite.svg'
+import heigLogo from './assets/heigLogo.svg'
 import './App.css'
 import axios from 'axios';
 
@@ -9,11 +8,23 @@ function App() {
   const [array, setArray] = useState([])
   const [sliderValue1, setSliderValue1] = useState(50); // Valeur initiale du slider
   const [sliderValue2, setSliderValue2] = useState(50); // Valeur initiale du slider
+  const [pictureFlag, setPictureFlag] = useState("");
 
   const fetchAPI = async () => {
     const response = await axios.get("http://localhost:8080/api/users");
     console.log(response.data.users);
     setArray(response.data.users);
+  };
+
+  const sendFlag = async () => {
+    try { const response = await axios.post("http://localhost:8080/take_picture", {
+        flag: "mon super flag",
+      });
+      setPictureFlag(response.data.pictureFlag);
+    } catch(error) {
+      console.error("Erreur :", error);
+      setPictureFlag("Erreur lors de l'envoi du flag");
+    }
   };
 
   useEffect(() => {
@@ -23,24 +34,21 @@ function App() {
   return (
     <>
       <div>
-        <a href="https://vite.dev" target="_blank">
-          <img src={viteLogo} className="logo" alt="Vite logo" />
-        </a>
-        <a href="https://react.dev" target="_blank">
-          <img src={reactLogo} className="logo react" alt="React logo" />
+        <a href="https://heig-vd.ch" target="_blank">
+          <img src={heigLogo} className="logo" alt="HEIG logo" />
         </a>
       </div>
-      <h1>Vite + React</h1>
+
+      <div>
+            <button onClick={sendFlag}>Envoyer le Flag</button>
+            {pictureFlag && <p>{pictureFlag}</p>}
+      </div>
+
+      <h1>Drone simulator</h1>
       <div className="card">
         <button onClick={() => setCount((count) => count + 1)}>
           count is {count}
         </button>
-          {array.map((user, index) => (
-            <div key={index}>
-              <span>{user}</span>
-              <br></br>
-            </div>
-          ))}
       </div>
 
       <div className="video-container">
@@ -73,10 +81,6 @@ function App() {
           <p>Valeur du slider : {sliderValue2}</p>
         </div>
       </div>
-
-      <p className="read-the-docs">
-        Click on the Vite and React logos to learn more
-      </p>
     </>
   )
 }
--- server/main.py
+++ server/main.py
@@ -1,4 +1,4 @@
-from flask import Flask, jsonify, send_from_directory, Response, render_template
+from flask import Flask, jsonify, send_from_directory, Response, render_template, request
 from flask_cors import CORS
 import cv2
 
@@ -20,6 +20,7 @@ def generate_frames():
             yield (b'--frame\r\n'
                    b'Content-Type: image/jpeg\r\n\r\n' + frame + b'\r\n')
 
+
 @app.route("/api/users", methods=['GET'])
 def users():
     return jsonify(
@@ -33,13 +34,48 @@ def users():
         }
     )
 
+
 @app.route('/video_feed')
 def video_feed():
     return Response(generate_frames(), mimetype='multipart/x-mixed-replace; boundary=frame')
 
+
 @app.route("/")
 def home():
     return send_from_directory(app.static_folder, "index.html")
+
+
+@app.route('/take_picture', methods=['POST'])
+def take_picture(): 
+    picture_flag = request.json
+    flag = picture_flag.get('flag')
+
+    if not flag:
+        return jsonify({"pictureFlag" : "Flag manquant"}), 400
+    
+    if flag == "mon super flag" :
+        # Capturer une image
+        ret, frame = camera.read()
+
+        # Vérifier si l'image a été capturée
+        if ret:
+            # Enregistrer l'image
+            cv2.imwrite('photo_webcam.jpg', frame)
+            print("Photo enregistrée avec succès")
+            
+            # Afficher l'image (optionnel)
+            cv2.imshow('Photo', frame)
+            cv2.waitKey(0)
+        else:
+            print("Erreur lors de la capture d'image")
+
+    # Libérer la webcam
+    #camera.release()
+    #cv2.destroyAllWindows()
+
+    print(f"Flag : {picture_flag.get('flag')}")
+    return jsonify({"pictureFlag" : "Flag reçu"}), 200
+
 
 if __name__ == "__main__":
     app.run(debug=True, port=8080)
